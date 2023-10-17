28fc33a82358fa6f719eef83db5250183dc72b23
Timofii0507 <timofiiyurkov@gmail.com>
2023-10-17 13:54:37 +0300

    Третє завдання  з дз

---
+++ main3.py
@@ -0,0 +1,9 @@
+num = int(input('Enter number: '))
+if num >0:
+    print('Number is positive')
+elif num < 0:
+    print('Number is negative')
+elif num == 0:
+    print('Number is equal to zero')
+else:
+    print('wrong syntax')
