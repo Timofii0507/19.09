453fc5b7aeeac32b13a8057d560d953250cd0f0e
Timofii0507 <timofiiyurkov@gmail.com>
2023-10-17 14:25:53 +0300

    П'яте завдання з практичної

---
+++ practive5.py
@@ -0,0 +1,11 @@
+hours = int(input("Введіть кількість годин: "))
+if 0 <= hours < 6:
+    print("Good Night")
+elif 6 <= hours < 13:
+    print("Good Morning")
+elif 13 <= hours < 17:
+    print("Good Day")
+elif 17 <= hours < 24:
+    print("Good Evening")
+else:
+    print("Некоректне введення годин. Введіть число від 0 до 23.")
